76ef248a7a3cd3b38a2cd932700c02e5d7a1b3a5
rickychongjl <61214361+rickychongjl@users.noreply.github.com>
2021-01-09 18:28:16 +1000

    removed unused imports

--- src/app/longwall/component/operator/operator.component.ts
+++ src/app/longwall/component/operator/operator.component.ts
@@ -3,7 +3,6 @@ import { ObservableArray } from '@nativescript/core';
 import { ShearerGraph } from '../../../shearer/shared/models/shearer-graph.model';
 import { ShearerService } from '../../../shearer/shared/shearer.service';
 import { ElectricityPriceInspectorService } from '../../../electricity-price-inspector/electricity-price-inspector.service';
-import * as dialogs from "tns-core-modules/ui/dialogs";
 
 @Component({
   selector: 'ns-operator',
@@ -84,14 +83,6 @@ export class OperatorComponent implements OnInit {
         this.shearerLocationArray.setItem(location.globalIndex, this.shearer);
 
       }
-    });
-  }
-
-  private notify(title: string, message: string, okText: string) {
-    dialogs.alert({
-      title: title,
-      message: message,
-      okButtonText: okText
     });
   }
 }
